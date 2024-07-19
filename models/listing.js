85d6e0d802d70956259953931c40290901a83e9f
piyush4596 <piyushprajapati3008@gmail.com>
2024-07-19 10:55:16 -0400

    Done all pages

--- models/listing.js
+++ models/listing.js
@@ -8,16 +8,17 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String,
-    url: {
-      type: String,
-      default:
-        "https://unsplash.com/photos/white-house-under-maple-trees-1ddol8rgUH8",
-      set: (v) =>
-        v === ""
-          ? "https://unsplash.com/photos/white-house-under-maple-trees-1ddol8rgUH8"
-          : v,
+    // filename: String,
+    // url: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1492889971304-ac16ab4a4a5a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: function (v) {
+      return v === ""
+        ? "https://images.unsplash.com/photo-1492889971304-ac16ab4a4a5a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v;
     },
+    // },
   },
   price: Number,
   location: String,
